4fffd23e2aeca07449abd1bdaa3341a5c59bd759
Huang Dong <u3008427@connect.hku.hk>
2024-11-08 16:34:55 +0800

    Add files via upload

--- AST_example.py
+++ AST_example.py
@@ -2,7 +2,7 @@ import ast
 import astor
 
 source_code = """
-def employability_level(education, age, experience):
+def employability_level(education: str, age: int, experience: int):
     score = 0
     if education in ['PhD', 'Masters']:
         score += 2
@@ -31,169 +31,169 @@ parsed_ast = ast.parse(source_code)
 print(ast.dump(parsed_ast, indent=4))
 
 
-Module(
-    body=[
-        FunctionDef(
-            name='employability_level',
-            args=arguments(
-                posonlyargs=[],
-                args=[
-                    arg(arg='education'),
-                    arg(arg='age'),
-                    arg(arg='experience')],
-                kwonlyargs=[],
-                kw_defaults=[],
-                defaults=[]),
-            body=[
-                Assign(
-                    targets=[
-                        Name(id='score', ctx=Store())],
-                    value=Constant(value=0)),
-                If(
-                    test=Compare(
-                        left=Name(id='education', ctx=Load()),
-                        ops=[
-                            In()],
-                        comparators=[
-                            List(
-                                elts=[
-                                    Constant(value='PhD'),
-                                    Constant(value='Masters')],
-                                ctx=Load())]),
-                    body=[
-                        AugAssign(
-                            target=Name(id='score', ctx=Store()),
-                            op=Add(),
-                            value=Constant(value=2))],
-                    orelse=[
-                        If(
-                            test=Compare(
-                                left=Name(id='education', ctx=Load()),
-                                ops=[
-                                    In()],
-                                comparators=[
-                                    List(
-                                        elts=[
-                                            Constant(value='Bachelor'),
-                                            Constant(value='Associate')],
-                                        ctx=Load())]),
-                            body=[
-                                AugAssign(
-                                    target=Name(id='score', ctx=Store()),
-                                    op=Add(),
-                                    value=Constant(value=1))],
-                            orelse=[])]),
-                If(
-                    test=BoolOp(
-                        op=And(),
-                        values=[
-                            Compare(
-                                left=Name(id='age', ctx=Load()),
-                                ops=[
-                                    GtE()],
-                                comparators=[
-                                    Constant(value=30)]),
-                            Compare(
-                                left=Name(id='age', ctx=Load()),
-                                ops=[
-                                    LtE()],
-                                comparators=[
-                                    Constant(value=50)])]),
-                    body=[
-                        AugAssign(
-                            target=Name(id='score', ctx=Store()),
-                            op=Add(),
-                            value=Constant(value=2))],
-                    orelse=[
-                        If(
-                            test=BoolOp(
-                                op=Or(),
-                                values=[
-                                    BoolOp(
-                                        op=And(),
-                                        values=[
-                                            Compare(
-                                                left=Name(id='age', ctx=Load()),
-                                                ops=[
-                                                    Gt()],
-                                                comparators=[
-                                                    Constant(value=50)]),
-                                            Compare(
-                                                left=Name(id='age', ctx=Load()),
-                                                ops=[
-                                                    LtE()],
-                                                comparators=[
-                                                    Constant(value=60)])]),
-                                    BoolOp(
-                                        op=And(),
-                                        values=[
-                                            Compare(
-                                                left=Name(id='age', ctx=Load()),
-                                                ops=[
-                                                    GtE()],
-                                                comparators=[
-                                                    Constant(value=20)]),
-                                            Compare(
-                                                left=Name(id='age', ctx=Load()),
-                                                ops=[
-                                                    Lt()],
-                                                comparators=[
-                                                    Constant(value=30)])])]),
-                            body=[
-                                AugAssign(
-                                    target=Name(id='score', ctx=Store()),
-                                    op=Add(),
-                                    value=Constant(value=1))],
-                            orelse=[])]),
-                If(
-                    test=Compare(
-                        left=Name(id='experience', ctx=Load()),
-                        ops=[
-                            GtE()],
-                        comparators=[
-                            Constant(value=5)]),
-                    body=[
-                        AugAssign(
-                            target=Name(id='score', ctx=Store()),
-                            op=Add(),
-                            value=Constant(value=2))],
-                    orelse=[
-                        AugAssign(
-                            target=Name(id='score', ctx=Store()),
-                            op=Add(),
-                            value=Constant(value=1))]),
-                If(
-                    test=Compare(
-                        left=Name(id='score', ctx=Load()),
-                        ops=[
-                            Gt()],
-                        comparators=[
-                            Constant(value=5)]),
-                    body=[
-                        Return(
-                            value=Constant(value='High Employability'))],
-                    orelse=[
-                        If(
-                            test=BoolOp(
-                                op=And(),
-                                values=[
-                                    Compare(
-                                        left=Name(id='score', ctx=Load()),
-                                        ops=[
-                                            Gt()],
-                                        comparators=[
-                                            Constant(value=3)]),
-                                    Compare(
-                                        left=Name(id='score', ctx=Load()),
-                                        ops=[
-                                            LtE()],
-                                        comparators=[
-                                            Constant(value=5)])]),
-                            body=[
-                                Return(
-                                    value=Constant(value='Medium Employability'))],
-                            orelse=[
-                                Return(
-                                    value=Constant(value='Low Employability'))])])],
-            decorator_list=[])],
-    type_ignores=[])
+# Module(
+#     body=[
+#         FunctionDef(
+#             name='employability_level',
+#             args=arguments(
+#                 posonlyargs=[],
+#                 args=[
+#                     arg(arg='education'),
+#                     arg(arg='age'),
+#                     arg(arg='experience')],
+#                 kwonlyargs=[],
+#                 kw_defaults=[],
+#                 defaults=[]),
+#             body=[
+#                 Assign(
+#                     targets=[
+#                         Name(id='score', ctx=Store())],
+#                     value=Constant(value=0)),
+#                 If(
+#                     test=Compare(
+#                         left=Name(id='education', ctx=Load()),
+#                         ops=[
+#                             In()],
+#                         comparators=[
+#                             List(
+#                                 elts=[
+#                                     Constant(value='PhD'),
+#                                     Constant(value='Masters')],
+#                                 ctx=Load())]),
+#                     body=[
+#                         AugAssign(
+#                             target=Name(id='score', ctx=Store()),
+#                             op=Add(),
+#                             value=Constant(value=2))],
+#                     orelse=[
+#                         If(
+#                             test=Compare(
+#                                 left=Name(id='education', ctx=Load()),
+#                                 ops=[
+#                                     In()],
+#                                 comparators=[
+#                                     List(
+#                                         elts=[
+#                                             Constant(value='Bachelor'),
+#                                             Constant(value='Associate')],
+#                                         ctx=Load())]),
+#                             body=[
+#                                 AugAssign(
+#                                     target=Name(id='score', ctx=Store()),
+#                                     op=Add(),
+#                                     value=Constant(value=1))],
+#                             orelse=[])]),
+#                 If(
+#                     test=BoolOp(
+#                         op=And(),
+#                         values=[
+#                             Compare(
+#                                 left=Name(id='age', ctx=Load()),
+#                                 ops=[
+#                                     GtE()],
+#                                 comparators=[
+#                                     Constant(value=30)]),
+#                             Compare(
+#                                 left=Name(id='age', ctx=Load()),
+#                                 ops=[
+#                                     LtE()],
+#                                 comparators=[
+#                                     Constant(value=50)])]),
+#                     body=[
+#                         AugAssign(
+#                             target=Name(id='score', ctx=Store()),
+#                             op=Add(),
+#                             value=Constant(value=2))],
+#                     orelse=[
+#                         If(
+#                             test=BoolOp(
+#                                 op=Or(),
+#                                 values=[
+#                                     BoolOp(
+#                                         op=And(),
+#                                         values=[
+#                                             Compare(
+#                                                 left=Name(id='age', ctx=Load()),
+#                                                 ops=[
+#                                                     Gt()],
+#                                                 comparators=[
+#                                                     Constant(value=50)]),
+#                                             Compare(
+#                                                 left=Name(id='age', ctx=Load()),
+#                                                 ops=[
+#                                                     LtE()],
+#                                                 comparators=[
+#                                                     Constant(value=60)])]),
+#                                     BoolOp(
+#                                         op=And(),
+#                                         values=[
+#                                             Compare(
+#                                                 left=Name(id='age', ctx=Load()),
+#                                                 ops=[
+#                                                     GtE()],
+#                                                 comparators=[
+#                                                     Constant(value=20)]),
+#                                             Compare(
+#                                                 left=Name(id='age', ctx=Load()),
+#                                                 ops=[
+#                                                     Lt()],
+#                                                 comparators=[
+#                                                     Constant(value=30)])])]),
+#                             body=[
+#                                 AugAssign(
+#                                     target=Name(id='score', ctx=Store()),
+#                                     op=Add(),
+#                                     value=Constant(value=1))],
+#                             orelse=[])]),
+#                 If(
+#                     test=Compare(
+#                         left=Name(id='experience', ctx=Load()),
+#                         ops=[
+#                             GtE()],
+#                         comparators=[
+#                             Constant(value=5)]),
+#                     body=[
+#                         AugAssign(
+#                             target=Name(id='score', ctx=Store()),
+#                             op=Add(),
+#                             value=Constant(value=2))],
+#                     orelse=[
+#                         AugAssign(
+#                             target=Name(id='score', ctx=Store()),
+#                             op=Add(),
+#                             value=Constant(value=1))]),
+#                 If(
+#                     test=Compare(
+#                         left=Name(id='score', ctx=Load()),
+#                         ops=[
+#                             Gt()],
+#                         comparators=[
+#                             Constant(value=5)]),
+#                     body=[
+#                         Return(
+#                             value=Constant(value='High Employability'))],
+#                     orelse=[
+#                         If(
+#                             test=BoolOp(
+#                                 op=And(),
+#                                 values=[
+#                                     Compare(
+#                                         left=Name(id='score', ctx=Load()),
+#                                         ops=[
+#                                             Gt()],
+#                                         comparators=[
+#                                             Constant(value=3)]),
+#                                     Compare(
+#                                         left=Name(id='score', ctx=Load()),
+#                                         ops=[
+#                                             LtE()],
+#                                         comparators=[
+#                                             Constant(value=5)])]),
+#                             body=[
+#                                 Return(
+#                                     value=Constant(value='Medium Employability'))],
+#                             orelse=[
+#                                 Return(
+#                                     value=Constant(value='Low Employability'))])])],
+#             decorator_list=[])],
+#     type_ignores=[])
